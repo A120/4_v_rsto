646b8ffc5d42c8097068af5dc755d838734ad269
Anze Sfiligoj <anze.sfiligoj@gmail.com>
2018-06-06 12:47:35 +0200

    Delal na GUI.

--- stiri_v_vrsto/src/GUI/Mislec.java
+++
@@ -1,8 +0,0 @@
-package GUI;
-
-public abstract class Mislec {
-	
-	public abstract void prekini();
-	public abstract void na_potezi();
-	//TODO
-}
--- stiri_v_vrsto/src/Logika_igre/Stanje.java
+++
@@ -1,9 +0,0 @@
-package Logika_igre;
-
-public enum Stanje {
-	NEODLOCENO,
-	ZMAGA_O,
-	ZMAGA_X,
-	NA_POTEZI_O,
-	NA_POTEZI_X;
-}
